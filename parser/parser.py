0f1898040eeea53f314146818661a59216b5160a
vinyteles <vinitelesufg@gmail.com>
2023-07-08 01:28:43 -0300

    refactor parser

--- parser/parser.py
+++ parser/parser.py
@@ -14,7 +14,7 @@ def open_file():
     go_to = pd.read_csv('parser/goto.csv')
     return action, go_to
 
-def find_on_go_to_table(rule):
+def find_rule(rule):
     return rules[rule]
 
 def stack_pop_beta_times(var_size_beta):
@@ -34,14 +34,24 @@ def print_rule(var_rule):
     print("")
     return None
 
+def stack_top():
+    return stack[-1]
+
+def find_action(action, s, a):
+    return action.loc[s].at[a["class"]]
+
+def find_go_to(go_to, t, var_rule):
+    return go_to.loc[t].at[var_rule[0]]
+
 def analysis(action, go_to):
     global s, t, stack
     a = scanner()
 
     while 1:
-        s = stack[-1]
-        var_action = action.loc[s].at[a["class"]]
+        s = stack_top()
+        var_action = find_action(action, s, a)
         print("s -> " + str(s) + ", aclass -> " + str(a["class"]) + ", varaction -> " + var_action)
+
         if var_action[0] == 'S':
             # empilhe p na pilha
             t = var_action[1:]
@@ -51,16 +61,16 @@ def analysis(action, go_to):
             a = scanner()
 
         elif var_action[0] == 'R':
-            var_rule = find_on_go_to_table(var_action[1:])
+            var_rule = find_rule(var_action[1:])
 
             #desempilha beta times
             var_size_beta = len(var_rule[1:])
             stack_pop_beta_times(var_size_beta)
 
             # faça t ser o topo da pilha
-            t = stack[-1]
+            t = stack_top()
             #empilhe goto[t, A]
-            var_goto = int(go_to.loc[t].at[var_rule[0]])
+            var_goto = int(find_go_to(go_to, t, var_rule[0]))
             stack.append(var_goto)
             print("go_to " + str(var_goto))
 
@@ -68,9 +78,11 @@ def analysis(action, go_to):
             print_rule(var_rule)
 
         elif var_action == "Acc":
+            # tirar o print e retornar None
             return "done"
 
         else:
+            # colocar o handler aqui
             return "eltin bota rotina de erro aqui pra nois"
 
 def parser():
